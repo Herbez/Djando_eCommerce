aaaa37214b431d93b55645dce4c3234fdf71f218
herbez <tshemaherbez@gmail.com>
2024-04-06 11:26:03 +0200

    login commit

--- e_shop/urls.py
+++ e_shop/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from django.conf.urls.static import static, settings
 
 urlpatterns = [
-    path('',views.index),
+    path('',views.index, name='index'),
+    path('login/',views.login_user, name='login'),
+    path('register/',views.register_user, name='register'),
+    path('logout/',views.logout_user, name='logout'),
+    path('about/',views.about, name='about'),
+    path('contact/',views.contact, name='contact'),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- e_shop/views.py
+++ e_shop/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 # Create your views here.
 
 products = Product.objects.all()
@@ -7,4 +11,34 @@ def index(request):
     return render(request, 'index.html',
                   {'products':products})
 
+def about(request):
+    return render(request, 'about.html', {})
 
+def contact(request):
+    return render(request, 'contact.html', {})
+
+def login_user(request):
+    
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request,username = username, password = password)
+        if user is not None:
+            login(request,user)
+            messages.success(request, ("You've been loggedin successfully! "))
+            return redirect('index')
+
+        else:
+            messages.success(request, ("Incorrect password or username! "))
+            return redirect('login')
+    
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You've been logged out"))
+    return redirect('index')
+
+def register_user(request):
+    return render(request, 'register.html', {})
